489f4b1426bbb05e3c29dde2192e789129f4afd4
nimbledy <nimbledy@gmail.com>
2024-06-11 16:50:11 +0700

    first commit

--- app/_app.tsx
+++
@@ -1,9 +0,0 @@
-// _app.tsx
-import { useClient } from 'next/data-client';
-
-function MyApp({ Component, pageProps }) {
-  const ComponentWithClient = useClient(Component); // Обернуть каждую страницу в useClient
-  return <ComponentWithClient {...pageProps} />;
-}
-
-export default MyApp;
---
+++ lib/clientComponents.ts
@@ -0,0 +1,35 @@
+// lib/clientComponents.ts
+
+import { useClient } from 'next/data-client';
+import { useState } from 'react';
+
+export function PageWithClient() {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const imageUrls = [
+    '/park-1.png',
+    '/park-2.png',
+    '/park-3.png',
+    '/park-4.png',
+  ];
+
+  const handlePrevImage = () => {
+    setCurrentImageIndex((prevIndex) => (prevIndex - 1 + imageUrls.length) % imageUrls.length);
+  };
+
+  const handleNextImage = () => {
+    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % imageUrls.length);
+  };
+
+  return (
+    <>
+      <h1>zizzamia.xyz</h1>
+      <img src={imageUrls[currentImageIndex]} alt="Park Image" style={{ aspectRatio: '1:1', width: '100%' }} />
+      <button onClick={handlePrevImage}>Previous Image</button>
+      <button onClick={handleNextImage}>Next Image</button>
+    </>
+  );
+}
+
+// Экспортируем обернутый компонент
+export default useClient(PageWithClient);
