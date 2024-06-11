1b2d32288b0f97058844c270bb7c40ff9a830f69
nimbledy <nimbledy@gmail.com>
2024-06-11 16:40:00 +0700

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
+import { useClient } from 'next/data-client';
 import { useState } from 'react';
 
-const imageUrls = [
-  '/park-1.png',
-  '/park-2.png',
-  '/park-3.png',
-  '/park-4.png',
-];
-
-export default function Page() {
+const Page = () => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const imageUrls = [
+    '/park-1.png',
+    '/park-2.png',
+    '/park-3.png',
+    '/park-4.png',
+  ];
 
   const handlePrevImage = () => {
     setCurrentImageIndex((prevIndex) => (prevIndex - 1 + imageUrls.length) % imageUrls.length);
@@ -20,10 +21,12 @@ export default function Page() {
 
   return (
     <>
-      <h1>Valera</h1>
+      <h1>zizzamia.xyz</h1>
       <img src={imageUrls[currentImageIndex]} alt="Park Image" style={{ aspectRatio: '1:1', width: '100%' }} />
       <button onClick={handlePrevImage}>Previous Image</button>
       <button onClick={handleNextImage}>Next Image</button>
     </>
   );
-}
+};
+
+export default useClient(Page);
